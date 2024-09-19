c6b990f79ca1a85ad7b2bae9247c1addfa28a326
kenziemhs <kenziemaheswara07@gmail.com>
2024-09-19 21:20:51 +0700

    update application page

--- src/pages/Application/index.jsx
+++ src/pages/Application/index.jsx
@@ -2,7 +2,168 @@
 
 const Application = () => {
   return (
-    <div>Application</div>
+
+    <section className="min-h-screen bg-black">
+
+      {/* section 1 */}
+      <div className="pt-20 relative h-screen grid w-full grid-cols-3 place-items-center gap-5">
+        <div className=" flex flex-col justify-center items-center">
+          <img src="/Generative AI_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+          <h1 className="text-white font-Poppins text-3xl">
+            Generative AI
+          </h1>
+
+        </div>
+        <div className=" flex flex-col justify-center items-center">
+          <img src="/public/Health Care_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+          <h1 className="text-white font-Poppins text-3xl">
+            Health Care
+          </h1>
+
+        </div>
+        <div className=" flex flex-col justify-center items-center">
+          <img src="/public/Finance_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+          <h1 className="text-white font-Poppins text-3xl">
+            Finance
+          </h1>
+
+        </div>
+        <div className=" flex flex-col justify-center items-center">
+          <img src="/public/Robotic_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+          <h1 className="text-white font-Poppins text-3xl">
+            Robotics
+          </h1>
+
+        </div>
+        <div className=" flex flex-col justify-center items-center">
+          <img src="/public/Entertain_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+          <h1 className="text-white font-Poppins text-3xl">
+            Entertainment
+          </h1>
+
+        </div>
+        <div className=" flex flex-col justify-center items-center">
+          <img src="/public/Social Media_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+          <h1 className="text-white font-Poppins text-3xl">
+            Social Media
+          </h1>
+
+
+
+        </div>
+      </div>
+      {/* section 2 */}
+      <div className="pt-20 relative h-screen grid w-full grid-cols-3 place-items-center gap-5">
+        <div className=" flex flex-col justify-center items-center">
+          <img src="/public/Agricultre_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+          <h1 className="text-white font-Poppins text-3xl">
+            Agriculture
+          </h1>
+
+        </div>
+        <div className=" flex flex-col justify-center items-center">
+          <img src="/public/Automobile_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+          <h1 className="text-white font-Poppins text-3xl">
+            Automobile
+          </h1>
+
+        </div>
+        <div className=" flex flex-col justify-center items-center">
+          <img src="/public/ChatBot_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+          <h1 className="text-white font-Poppins text-3xl">
+            Chatbot
+          </h1>
+
+        </div>
+        <div className=" flex flex-col justify-center items-center">
+          <img src="/public/Comp Vision_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+          <h1 className="text-white font-Poppins text-3xl">
+            Computer Vision
+          </h1>
+
+        </div>
+        <div className=" flex flex-col justify-center items-center">
+          <img src="/public/Education.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+          <h1 className="text-white font-Poppins text-3xl">
+            Education
+          </h1>
+
+
+        </div>
+        <div className=" flex flex-col justify-center items-center">
+          <img src="/public/Face Recognition_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+          <h1 className="text-white font-Poppins text-3xl">
+            Face Recognition
+          </h1>
+
+        </div>
+      </div>
+
+      {/* section 3 */}
+      <div className="w-full h-screen relative bg-black">
+        <div className="pt-20 relative h-screen grid w-full grid-cols-3 place-items-center gap-5">
+          <div className=" flex flex-col justify-center items-center">
+            <img src="/Generative AI_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+            <h1 className="text-white font-Poppins text-3xl">
+              Generative AI
+            </h1>
+
+          </div>
+          <div className=" flex flex-col justify-center items-center">
+            <img src="/public/Health Care_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+            <h1 className="text-white font-Poppins text-3xl">
+              Health Care
+            </h1>
+
+          </div>
+          <div className=" flex flex-col justify-center items-center">
+            <img src="/public/Finance_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+            <h1 className="text-white font-Poppins text-3xl">
+              Finance
+            </h1>
+
+          </div>
+          <div className=" flex flex-col justify-center items-center">
+            <img src="/public/Robotic_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+            <h1 className="text-white font-Poppins text-3xl">
+              Robotics
+            </h1>
+
+          </div>
+          <div className=" flex flex-col justify-center items-center">
+            <img src="/public/Entertain_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+            <h1 className="text-white font-Poppins text-3xl">
+              Entertainment
+            </h1>
+
+          </div>
+          <div className=" flex flex-col justify-center items-center">
+            <img src="/public/Social Media_App.svg" alt="GambarAI" className="w-[232.3px] h=[196.2px]" />
+
+            <h1 className="text-white font-Poppins text-3xl">
+              Social Media
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
