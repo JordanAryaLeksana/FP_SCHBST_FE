fff6ae8aa3cb33ea3def234344ad4539e1aff65a
girpanns <girvannotario985@gmail.com>
2024-09-19 21:03:59 +0700

    update layout keytech

--- src/pages/KeyTech/index.jsx
+++ src/pages/KeyTech/index.jsx
@@ -3,8 +3,55 @@
 
 const KeyTech = () => {
   return (
-    <div>KeyTech</div>
+    <section className="min-h-screen bg-black">
+      <div className=" relative h-screen w-full flex justify-center item-center pl-20 pr-20">
+        <div className="pt-20 flex flex-col w-1/2 ">
+          <h1 className="text-white font-Poppins text-4xl">
+            Machine Learning
+          </h1>
+          <img src="/Machine Learnng.svg" alt="" className="w-[400px] h-[200px] ">
+          </img>
+          <p className="text-white font-Inter text-base">
+          cabang dari kecerdasan buatan yang fokus pada pengembangan algoritma dan model yang memungkinkan komputer untuk belajar dari data dan membuat keputusan atau prediksi tanpa pemrograman eksplisit. Dalam pembelajaran mesin, sistem dilatih menggunakan data historis untuk mengenali pola dan hubungan, sehingga dapat menggeneralisasi informasi baru. Proses ini melibatkan beberapa langkah, termasuk pengumpulan data, pra-pemrosesan, pelatihan model, dan evaluasi hasil.
+          </p>
+        </div>
+
+        <div className="pt-20 flex flex-col w-1/2">
+          <h2 className="text-white font-poppins text-4xl">
+            Deep Learning
+          </h2>
+          <img src="Deep Learning_KeyTech.svg" alt="" className="w-[400px] h-[200px] ">
+          </img>
+         
+        </div>
+      </div>
+
+      <div className=" relative h-screen w-full flex justify-center item-center pl-20 pr-20">
+        <div className="pt-20 flex flex-col w-1/2 ">
+          <h1 className="text-white font-Poppins text-4xl">
+            Machine Learning
+          </h1>
+          <img src="/Machine Learnng.svg" alt="" className="w-[400px] h-[200px] ">
+          </img>
+          <p className="text-white font-Inter text-base">
+          cabang dari kecerdasan buatan yang fokus pada pengembangan algoritma dan model yang memungkinkan komputer untuk belajar dari data dan membuat keputusan atau prediksi tanpa pemrograman eksplisit. Dalam pembelajaran mesin, sistem dilatih menggunakan data historis untuk mengenali pola dan hubungan, sehingga dapat menggeneralisasi informasi baru. Proses ini melibatkan beberapa langkah, termasuk pengumpulan data, pra-pemrosesan, pelatihan model, dan evaluasi hasil.
+          </p>
+        </div>
+
+        <div className="pt-20 flex flex-col w-1/2">
+          <h2 className="text-white font-poppins text-4xl">
+            Deep Learning
+          </h2>
+          <img src="Deep Learning_KeyTech.svg" alt="" className="w-[400px] h-[200px] ">
+          </img>
+
+        </div>
+      </div>
+    </section>
+    
   )
 }
+
+
 
 export default KeyTech
