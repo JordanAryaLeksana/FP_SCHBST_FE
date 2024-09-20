de70a374c46285619d82ca957c0aba25b1505f72
girpanns <girvannotario985@gmail.com>
2024-09-20 14:19:47 +0700

    updates/keytech

--- src/pages/KeyTech/index.jsx
+++ src/pages/KeyTech/index.jsx
@@ -1,54 +1,160 @@
-
-
-
 const KeyTech = () => {
   return (
     <section className="min-h-screen bg-black">
       <div className=" relative h-screen w-full flex justify-center item-center pl-20 pr-20">
-        <div className="pt-20 flex flex-col w-1/2 ">
-          <h1 className="text-white font-Poppins text-4xl">
+        <div className="pt-20 flex gap-5 flex-col w-1/2 pr-10 mb-4 ">
+          <h1 className="text-white font-serif text-4xl underline">
             Machine Learning
           </h1>
-          <img src="/Machine Learnng.svg" alt="" className="w-[400px] h-[200px] ">
+          <img src="/Machine Learn.png" alt="" className="w-[502.8px] h-[245.1px] rounded-3xl ">
           </img>
-          <p className="text-white font-Inter text-base">
-          cabang dari kecerdasan buatan yang fokus pada pengembangan algoritma dan model yang memungkinkan komputer untuk belajar dari data dan membuat keputusan atau prediksi tanpa pemrograman eksplisit. Dalam pembelajaran mesin, sistem dilatih menggunakan data historis untuk mengenali pola dan hubungan, sehingga dapat menggeneralisasi informasi baru. Proses ini melibatkan beberapa langkah, termasuk pengumpulan data, pra-pemrosesan, pelatihan model, dan evaluasi hasil.
+          <p className="text-white font-Inter text-sm whitespace-normal text-justify">
+          Cabang dari kecerdasan buatan yang fokus pada pengembangan algoritma
+           dan model yang memungkinkan komputer untuk belajar dari data dan
+            membuat keputusan atau prediksi tanpa pemrograman eksplisit. Dalam
+             pembelajaran mesin, sistem dilatih menggunakan data historis untuk
+              mengenali pola dan hubungan, sehingga dapat menggeneralisasi informasi
+               baru. Proses ini melibatkan beberapa langkah, termasuk pengumpulan data,
+                pra-pemrosesan, pelatihan model, dan evaluasi hasil.
+          </p>
+          <p className="text-white font-Inter text-sm">
+          Machine Learning Platform<br></br>
+          <li>PyTorch</li>
+          <li>Tensorflow</li>
+          <li>Scikit-Learn</li>
+          <li>Microsoft Azure</li>
+          <li>Amazon SageMaker</li>
+          <li>Google Cloud</li>
           </p>
         </div>
 
-        <div className="pt-20 flex flex-col w-1/2">
-          <h2 className="text-white font-poppins text-4xl">
+
+        <div className="pt-20 flex flex-col gap-5 w-1/2 pl-10">
+          <h2 className="text-white font-poppins text-4xl underline">
             Deep Learning
           </h2>
-          <img src="Deep Learning_KeyTech.svg" alt="" className="w-[400px] h-[200px] ">
+          <img src="DeepLearn_KeyTech.png" alt="Deep Learning" className="w-[502.8px] h-[245.1px] rounded-3xl ">
           </img>
-         <p>
-          halo
+         <p className="text-white font-Inter text-sm whitespace-normal text-justify">
+         Subfield dari pembelajaran mesin yang menggunakan jaringan saraf tiruan
+          dengan banyak lapisan (deep neural networks) untuk menganalisis data dan
+           melakukan tugas kompleks. Berbeda dengan teknik pembelajaran mesin
+            tradisional, deep learning mampu otomatis mengekstrak fitur dari data
+             mentah, seperti gambar, suara, atau teks, tanpa memerlukan pengolahan
+              manual yang intensif.
          </p>
+         <p className="text-white font-Inter text-sm">
+          Deep Learning Platform<br></br>
+          <li>PyTorch</li>
+          <li>Tensorflow</li>
+          <li>Scikit-Learn</li>
+          <li>Microsoft Azure</li>
+          <li>Amazon SageMaker</li>
+          <li>IBM Watsons Studio</li>
+          </p>
         </div>
       </div>
+     
 
-      <div className=" relative h-screen w-full flex justify-center item-center pl-20 pr-20">
-        <div className="pt-20 flex flex-col w-1/2 ">
-          <h1 className="text-white font-Poppins text-4xl">
-            Machine Learning
+      <div className=" relative h-screen w-full flex justify-center item-center pl-20 pr-20 mt-20">
+        <div className="pt-20 flex flex-col w-1/2 pr-10 gap-5">
+          <h1 className="text-white font-serif text-4xl underline ">
+            Natural Language Prosessing
           </h1>
-          <img src="/Machine Learnng.svg" alt="" className="w-[400px] h-[200px] ">
+          <img src="/NLP_KeyTech.png" alt="Natural Language Proecessing" className="w-[502.8px] h-[245.1px] rounded-3xl">
           </img>
-          <p className="text-white font-Inter text-base">
-          cabang dari kecerdasan buatan yang fokus pada pengembangan algoritma dan model yang memungkinkan komputer untuk belajar dari data dan membuat keputusan atau prediksi tanpa pemrograman eksplisit. Dalam pembelajaran mesin, sistem dilatih menggunakan data historis untuk mengenali pola dan hubungan, sehingga dapat menggeneralisasi informasi baru. Proses ini melibatkan beberapa langkah, termasuk pengumpulan data, pra-pemrosesan, pelatihan model, dan evaluasi hasil.
+          <p className="text-white font-inter text-sm text-justify">
+          Cabang dari kecerdasan buatan yang fokus pada interaksi antara komputer dan bahasa manusia.
+           NLP menggabungkan ilmu komputer, linguistik, dan kecerdasan buatan untuk memungkinkan mesin
+            memahami, memproses, dan menghasilkan bahasa alami dalam bentuk yang dapat dipahami oleh manusia.
+          </p>
+          <p className="text-white font-Inter text-sm">
+          NLP Platform<br></br>
+        <li>Google Cloud NLP API</li>
+        <li>IBM Watson Undersatanding</li>
+        <li>Microsoft Azure</li>
+        <li>Amazon Comprehend</li>
+        <li>Spacy</li> 
+        <li>NLTK</li> 
+        <li>Hugging Face</li>
           </p>
         </div>
 
-        <div className="pt-20 flex flex-col w-1/2">
-          <h2 className="text-white font-poppins text-4xl">
-            Deep Learning
+        <div className="pt-20 flex flex-col w-1/2 pl-10 gap-5">
+          <h2 className="text-white font-serif text-4xl underline">
+            Computer Vision
           </h2>
-          <img src="Deep Learning_KeyTech.svg" alt="" className="w-[400px] h-[200px] ">
+          <img src="CompVision_KeyTech.png" alt="" className="w-[5028px] h-[245.1px] rounded-3xl ">
           </img>
+          <p className="text-white font-Inter text-sm whitespace-normal text-justify">
+          Bidang kecerdasan buatan yang berfokus pada pengembangan algoritma dan teknik yang memungkinkan 
+          komputer untuk memahami dan memproses informasi visual dari dunia di sekitar mereka. Dengan menggunakan gambar 
+          dan video, computer vision bertujuan untuk meniru kemampuan manusia dalam melihat dan memahami visual.
+          </p>
+          <p className="text-white font-Inter text-sm">
+          Computer Vision Platform<br></br>
+        <li>OpenCV</li>
+        <li>Tensorflow</li>
+        <li>Microsoft Azure</li>
+        <li>Amazon Comprehend</li>
+        <li>Spacy</li> 
+        <li>NLTK</li> 
+        <li>Hugging Face</li>
+          </p>
 
         </div>
       </div>
+
+      <div className=" relative h-screen w-full flex justify-center item-center pl-20 pr-20 mt-10">
+        <div className="pt-20 flex flex-col w-1/2 pr-10 gap-5">
+          <h1 className="text-white font-serif text-4xl underline ">
+            Expert System
+          </h1>
+          <img src="/Expert System_KeyTech.png" alt="Natural Language Proecessing" className="w-[502.8px] h-[245.1px] rounded-3xl">
+          </img>
+          <p className="text-white font-inter text-sm text-justify">
+          Program komputer yang dirancang untuk meniru kemampuan pengambilan keputusan seorang ahli
+           dalam bidang tertentu. Sistem ini menggunakan basis pengetahuan yang terdiri dari fakta dan
+            aturan, serta algoritma inferensi untuk memecahkan masalah yang kompleks atau memberikan rekomendasi.
+          </p>
+          <p className="text-white font-Inter text-sm">
+          Expert System Platform<br></br>
+        <li>IBM Watson</li>
+        <li>MYCIN</li>
+        <li>CLIPS</li>
+        <li>Dendral</li>
+        <li>XPart</li> 
+        <li>Zylab</li> 
+        <li>KNIME</li>
+          </p>
+        </div>
+
+        <div className="pt-20 flex flex-col w-1/2 pl-10 gap-5">
+          <h2 className="text-white font-serif text-4xl underline">
+          Generative Adversarial Network
+          </h2>
+          <img src="GAN_KeyTech.png" alt="" className="w-[5028px] h-[245.1px] rounded-3xl ">
+          </img>
+          <p className="text-white font-Inter text-sm whitespace-normal text-justify">
+          Suatu jenis arsitektur jaringan saraf yang digunakan dalam pembelajaran mesin,
+           yang terdiri dari dua model yang saling bersaing: generator dan discriminator.
+            Konsep ini diperkenalkan oleh Ian Goodfellow dan rekan-rekannya pada tahun 2014.
+          </p>
+          <p className="text-white font-Inter text-sm">
+          Generative Adversarial Network Platform<br></br>
+        <li>Tensorflow</li>
+        <li>Pytorch</li>
+        <li>Keras</li>
+        <li>NVIDIA GANVerse3D</li>
+        <li>Hugging Face Transformers</li> 
+        <li>Runway ML</li> 
+        <li>DeepArt</li>
+          </p>
+
+        </div>
+      </div>
+
+      <div className=" relative h-screen w-full flex justify-center item-center pl-20 pr-20 mt-10"></div>
     </section>
     
   )
@@ -56,4 +162,4 @@ const KeyTech = () => {
 
 
 
-export default KeyTech
+export default KeyTech
